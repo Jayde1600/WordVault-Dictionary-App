68e7611f33c9c910ecf0d5880c651e62d3f4af9c
Jayde1600 <neomeslane@gmail.com>
2024-03-13 09:16:02 +0200

    fixed the search bar error, also add loading bar

--- app/src/main/java/com/example/wordvault/MainActivity.kt
+++ app/src/main/java/com/example/wordvault/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.wordvault
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import com.example.wordvault.databinding.ActivityMainBinding
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -17,19 +18,34 @@ private lateinit var binding: ActivityMainBinding
         setContentView(binding.root)
 
         binding.SearchBtn.setOnClickListener {
-            val word = binding.SearchBtn.text.toString()
+            val word = binding.SearchInput.text.toString()
             getMeaning(word)
         }
 
     }
 
         private fun getMeaning(word : String) {
+            setInProgress(true)
             GlobalScope.launch {
                 val response = RetrofitInstance.dictionaryAPI.getMeaning(word)
-                Log.i("Response from API", response.body().toString())
+                runOnUiThread {
+                    setInProgress(false)
+                }
             }
-
     }
 
+    private fun setInProgress(InProgress : Boolean) {
+        if(InProgress) {
+            binding.SearchBtn.visibility = View.INVISIBLE
+            binding.progressBar.visibility = View.VISIBLE
+        }
+        else {
+            binding.SearchBtn.visibility = View.VISIBLE
+            binding.progressBar.visibility = View.INVISIBLE
+        }
+    }
 
 }
+
+
+
