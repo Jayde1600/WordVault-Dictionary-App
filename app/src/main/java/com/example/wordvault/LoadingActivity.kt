10000cd1b0e7eba0f623bbba934cc7fb6468f525
Jayde1600 <neomeslane@gmail.com>
2024-03-21 12:10:58 +0200

    Added a splash screen to the app

---
+++ app/src/main/java/com/example/wordvault/LoadingActivity.kt
@@ -0,0 +1,18 @@
+package com.example.wordvault
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class LoadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 5000) // 3000 milliseconds delay
+    }
+}
